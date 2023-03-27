1ef3863aec2395205bf4c9745193824a54b90665
Guilherme-Avellar <85587714+Guilherme-Avellar@users.noreply.github.com>
2023-03-27 15:36:00 -0300

    Add files via upload

---
+++ intro01_io.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // printf("Hello World\n");
+    cout << "Hello World\n";
+    cout << "Hello World" << "\n";
+    cout << "Hello World" << endl;
+
+    int a;
+    cin >> a; // scanf("%d", &a);
+    cout << a << endl; // printf("%d", a);
+
+    return 0;
+}
